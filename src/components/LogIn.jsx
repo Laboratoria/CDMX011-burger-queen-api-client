58167750f71cd9d565a83d48b495a854b589d7c8
AngieVenta <angelicaventap@gmail.com>
2021-11-08 21:00:45 -0600

    SignUp and LogIn functions added, also Router
    Co-authored-by: DianaAraujoG <dianac251@gmail.com>
    Co-authored by: AngieVenta <angelicaventap@gmail.com>

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,3 +1,52 @@
-// import React, {Component} from "react";
-// import app from '../FirebaseConfig'
+import React, { Fragment, useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../FirebaseConfig';
 
+function LogIn (){
+    const [loginMail, setLoginMail] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+
+    const logInPromise = async (event) =>{
+        event.preventDefault();
+        try{
+            const user = await signInWithEmailAndPassword(
+                auth,
+                loginMail,
+                loginPassword
+            );
+            console.log(user);
+        }catch(error){
+            console.log(error.message);
+        }
+    }
+
+    return(
+        <Fragment>
+            <h1>Iniciar sesion</h1>
+            <form>
+                <label> Correo Electrónico: </label>
+                <input
+                 type='email'
+                 placeholder='Ingresa tu email'
+                 className= 'formInput'
+                 onChange={(event) => {
+                    setLoginMail(event.target.value);
+                  }}></input>
+                 <label> Contraseña: </label>
+                 <input
+                 type='password'
+                 placeholder='Ingrese su contraseña'
+                 className= 'formInput'
+                 onChange={(event) => {
+                    setLoginPassword(event.target.value);
+                  }}></input>
+                 <button 
+                 className= 'buttonForm'
+                 type='submit'
+                 onClick={logInPromise}>Enviar</button>
+            </form>
+        </Fragment>    
+    )
+}
+
+export default LogIn
--- src/components/SingUp.jsx
+++ src/components/SingUp.jsx
@@ -1,30 +1,66 @@
-import React, { Fragment } from 'react';
-//import { createUserWithEmailAndPassword } from 'firebase/auth';
-//import { auth } from '../FirebaseConfig';
-// import app from '../FirebaseConfig'
+import React, { Fragment, useState } from 'react';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { auth } from '../FirebaseConfig';
 
 function SignUp () {
 
+    const [signMail, setsignMail] = useState('');
+    const [signPassword, setsignPassword] = useState('');
+    const [signUsername, setsignUsername] = useState('');
+    
+    const signPromise = async (event) => {  
+        event.preventDefault();      
+        try{
+            const user = await createUserWithEmailAndPassword(
+                auth,
+                signMail,
+                signPassword
+            );
+            
+            await updateProfile(auth.currentUser,{
+                displayName: signUsername
+            })
+            console.log(user);
+        } catch (error){
+            console.log(error.message)
+        }
+    }
+    
     return (
         <Fragment>
             <div className = 'signUp'>
              <h1>Regístrate</h1>
-             <form className ='signUpForm'>
+             <form className ='signUpForm' onSubmit={signPromise}>
+                <label> Nombre: </label>
+                 <input
+                 type='text'
+                 placeholder='Ingresa tu numbre'
+                 id= 'formUsername'
+                 className= 'formInput'
+                 onChange={(event) => {
+                    setsignUsername(event.target.value);
+                  }}></input>
                  <label> Correo Electrónico: </label>
                  <input
                  type='email'
                  placeholder='Ingresa tu email'
                  id= 'formEmail'
-                 className= 'emailInput'></input>
+                 className= 'formInput'
+                 onChange={(event) => {
+                    setsignMail(event.target.value);
+                  }}></input>
                  <label> Contraseña: </label>
                  <input
                  type='password'
                  placeholder='Ingresa una contraseña válida'
                  id= 'formPassword'
-                 className= 'passwordInput'></input>
+                 className= 'formInput'
+                 onChange={(event) => {
+                    setsignPassword(event.target.value);
+                  }}></input>
                  <button 
-                 type= 'submit'
-                 className= 'buttonForm'>Registrarse</button>
+                 className= 'buttonForm'
+                 type='submit'>Registrarse</button>
              </form>
             </div>
         </Fragment>
